f4a47bb027c51b42dcb4da1d5a900ba3c8fb19dd
pepeyukke <amukat1201.ta@gmail.com>
2025-07-01 07:49:39 +0900

    calc 2.0

--- commands/calculator.go
+++ commands/calculator.go
@@ -3,11 +3,53 @@ package commands
 import (
 	"fmt"
 	"luna/logger"
+	"math"
 	"strconv"
 
 	"github.com/Knetic/govaluate"
 	"github.com/bwmarrin/discordgo"
 )
+
+var functions = map[string]govaluate.ExpressionFunction{
+	// --- 三角関数 ---
+	"sin": func(args ...interface{}) (interface{}, error) {
+		return math.Sin(args[0].(float64)), nil
+	},
+	"cos": func(args ...interface{}) (interface{}, error) {
+		return math.Cos(args[0].(float64)), nil
+	},
+	"tan": func(args ...interface{}) (interface{}, error) {
+		return math.Tan(args[0].(float64)), nil
+	},
+	// --- 基本的な数学関数 ---
+	"sqrt": func(args ...interface{}) (interface{}, error) {
+		return math.Sqrt(args[0].(float64)), nil
+	},
+	"abs": func(args ...interface{}) (interface{}, error) {
+		return math.Abs(args[0].(float64)), nil
+	},
+	"log": func(args ...interface{}) (interface{}, error) {
+		return math.Log(args[0].(float64)), nil
+	},
+	"log10": func(args ...interface{}) (interface{}, error) {
+		return math.Log10(args[0].(float64)), nil
+	},
+	"ceil": func(args ...interface{}) (interface{}, error) {
+		return math.Ceil(args[0].(float64)), nil
+	},
+	"floor": func(args ...interface{}) (interface{}, error) {
+		return math.Floor(args[0].(float64)), nil
+	},
+	"round": func(args ...interface{}) (interface{}, error) {
+		return math.Round(args[0].(float64)), nil
+	},
+	"max": func(args ...interface{}) (interface{}, error) {
+		return math.Max(args[0].(float64), args[1].(float64)), nil
+	},
+	"min": func(args ...interface{}) (interface{}, error) {
+		return math.Min(args[0].(float64), args[1].(float64)), nil
+	},
+}
 
 func init() {
 	cmd := &discordgo.ApplicationCommand{
@@ -25,12 +67,9 @@ func init() {
 
 	handler := func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		logger.Info.Println("calc command received")
-
-		// ユーザーが入力した数式を取得
 		expressionStr := i.ApplicationCommandData().Options[0].StringValue()
 
-		// govaluateを使って数式を評価
-		expression, err := govaluate.NewEvaluableExpression(expressionStr)
+		expression, err := govaluate.NewEvaluableExpressionWithFunctions(expressionStr, functions)
 		if err != nil {
 			logger.Error.Printf("Failed to parse expression: %v", err)
 			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
@@ -43,7 +82,10 @@ func init() {
 			return
 		}
 
-		result, err := expression.Evaluate(nil)
+		parameters := make(map[string]interface{}, 8)
+		parameters["pi"] = math.Pi
+
+		result, err := expression.Evaluate(parameters)
 		if err != nil {
 			logger.Error.Printf("Failed to evaluate expression: %v", err)
 			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
@@ -56,26 +98,17 @@ func init() {
 			return
 		}
 
-		// 結果をきれいにフォーマット
-		// govaluateはfloat64で結果を返すので、整数なら小数点以下を消す
 		resultStr := strconv.FormatFloat(result.(float64), 'f', -1, 64)
 
-		// 結果表示用のEmbedを作成
 		embed := &discordgo.MessageEmbed{
 			Author: &discordgo.MessageEmbedAuthor{
 				Name:    i.Member.User.Username,
 				IconURL: i.Member.User.AvatarURL(""),
 			},
-			Color: 0x2ECC71, // 緑色
+			Color: 0x2ECC71,
 			Fields: []*discordgo.MessageEmbedField{
-				{
-					Name:  "問題",
-					Value: fmt.Sprintf("```%s```", expressionStr),
-				},
-				{
-					Name:  "答え",
-					Value: fmt.Sprintf("```%s```", resultStr),
-				},
+				{Name: "問題", Value: fmt.Sprintf("```%s```", expressionStr)},
+				{Name: "答え", Value: fmt.Sprintf("```%s```", resultStr)},
 			},
 		}
 
