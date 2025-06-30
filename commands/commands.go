f9ead665a20f98108f836f722445760005928737
pepeyukke <amukat1201.ta@gmail.com>
2025-07-01 04:58:50 +0900

    environment variable fix

--- commands/commands.go
+++ commands/commands.go
@@ -2,10 +2,18 @@ package commands
 
 import "github.com/bwmarrin/discordgo"
 
+// コマンド登録用の変数
 var Commands = []*discordgo.ApplicationCommand{}
 var CommandHandlers = make(map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate))
 
-// この関数をここに一ヶ所だけ定義
+// チケット機能用の共有変数
+var (
+	ticketStaffRoleID = make(map[string]string)
+	ticketCategoryID  = make(map[string]string)
+	ticketCounter     = make(map[string]int)
+)
+
+// 共有ヘルパー関数
 func int64Ptr(i int64) *int64 {
 	return &i
 }
