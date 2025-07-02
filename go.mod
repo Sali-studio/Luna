fd555c03bba8467e5ef4799a1da5c55023004778
pepeyukke <amukat1201.ta@gmail.com>
2025-07-02 20:25:18 +0900

    Add lumberjack dependencies to go.mod and go.sum and update logger package to implement structured logging

--- go.mod
+++ go.mod
@@ -12,4 +12,5 @@ require (
 	github.com/gorilla/websocket v1.5.3 // indirect
 	golang.org/x/crypto v0.39.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 )
--- logger/logger.go
+++ logger/logger.go
@@ -1,25 +1,60 @@
 package logger
 
 import (
-	"log"
+	"log/slog"
 	"os"
+
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
-var (
-	Info    *log.Logger
-	Warning *log.Logger
-	Error   *log.Logger
-	Fatal   *log.Logger
-)
+// シングルトンとしてロガーを保持
+var logger *slog.Logger
 
 func Init() {
-	file, err := os.OpenFile("luna.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		log.Fatal(err)
+	// ログローテーションの設定
+	logFile := &lumberjack.Logger{
+		Filename:   "luna.log", // ログファイル名
+		MaxSize:    10,         // 1ファイルあたりの最大サイズ (MB)
+		MaxBackups: 5,          // 保持する古いログの最大数
+		MaxAge:     30,         // 古いログを保持する最大日数
+		Compress:   true,       // 古いログをgzipで圧縮
 	}
 
-	Info = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-	Warning = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
-	Error = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
-	Fatal = log.New(file, "FATAL: ", log.Ldate|log.Ltime|log.Lshortfile)
+	// ログの出力先を「標準出力（コンソール）」と「ファイル」の両方に設定
+	multiWriter := os.Stdout
+	if logFile != nil {
+		multiWriter = os.MultiWriter(os.Stdout, logFile)
+	}
+
+	// 新しい構造化ロガーを作成
+	logger = slog.New(slog.NewJSONHandler(multiWriter, &slog.HandlerOptions{
+		AddSource: true,            // ソースコードのファイル名と行番号を追加
+		Level:     slog.LevelDebug, // DEBUGレベル以上のログをすべて記録
+	}))
+}
+
+// --- 新しいログ出力関数 ---
+
+// Infoレベルのログを出力
+// 例: logger.Info("Botが起動しました", "version", "1.2.3")
+func Info(msg string, args ...any) {
+	logger.Info(msg, args...)
+}
+
+// Warnレベルのログを出力
+// 例: logger.Warn("APIキーが設定されていません", "env", "GEMINI_API_KEY")
+func Warn(msg string, args ...any) {
+	logger.Warn(msg, args...)
+}
+
+// Errorレベルのログを出力
+// 例: logger.Error("コマンドの実行に失敗", "error", err, "command", "ping")
+func Error(msg string, args ...any) {
+	logger.Error(msg, args...)
+}
+
+// Fatalレベルのログを出力（出力後にプログラムを終了）
+func Fatal(msg string, args ...any) {
+	logger.Error(msg, args...)
+	os.Exit(1)
 }
